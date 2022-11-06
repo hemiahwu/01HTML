ba712a91c89721890c51505f4012a989a493aa11
hemiahwu <309595700@qq.com>
2022-11-06 13:02:52 +0800

    01html

---
+++ 04-css专题进阶/12css-cssvar/main.js
@@ -0,0 +1,9 @@
+// #06c .box
+const box = document.querySelector(".box");
+const boxStyles = getComputedStyle(box);
+// console.log(boxStyles);
+const boxBorderColor = boxStyles.getPropertyValue("--box-border-color");
+// console.log(boxBorderColor);
+
+const header = document.querySelector("#main-header");
+header.style.setProperty("--header-bg-color","green");
